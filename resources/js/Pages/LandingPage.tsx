056b7d56593e72739a981cd29d7b45371ab73906
Retno Pinka <retnopinka20@gmail.com>
2025-03-22 01:35:18 +0700

    feat: customer navbar

---
+++ resources/js/Pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import CustomerLayout from "@/Layouts/CustomerLayout";
+
+export default function LandingPage() {
+    return (
+        <>
+            <CustomerLayout>
+                <p>Ini halaman landing page</p>
+            </CustomerLayout>
+        </>
+    );
+}
