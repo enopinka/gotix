27cb2d31677224e04466bbfad8381e81cd5d625e
Retno Pinka <retnopinka20@gmail.com>
2025-06-23 02:28:10 +0700

    Add new icon and update manifest with new assets

---
+++ public/build/assets/StatCard-CvISanz1.js
@@ -0,0 +1,6 @@
+import{c}from"./createLucideIcon-Cvm-H_v4.js";import{j as e}from"./app-BIx_ZH_8.js";/**
+ * @license lucide-react v0.479.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8",key:"1h4pet"}],["path",{d:"M12 18V6",key:"zqpxq5"}]],d=c("CircleDollarSign",l);function n({icon:s,title:t,value:a}){return e.jsxs("div",{className:"bg-white shadow-md rounded-xl p-5 flex items-center space-x-4",children:[e.jsx("div",{className:"bg-blue-100 text-blue-600 rounded-full p-3",children:s}),e.jsxs("div",{children:[e.jsx("p",{className:"text-sm text-gray-500",children:t}),e.jsx("p",{className:"text-xl font-bold",children:a})]})]})}export{d as C,n as S};
